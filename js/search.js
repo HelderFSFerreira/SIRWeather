46a6d3321ee65856fed320cadc93c50caf15e6db
Helder Ferreira <woounn@gmail.com>
2016-12-20 21:03:27 +0000

    minor changes

--- js/search.js
+++ js/search.js
@@ -131,14 +131,3 @@ function forecast(cidade) {
     }
   })
 }
-/*<div class="forecast" id="forecast6">
-  <div class="forecast-header">
-    <div class="day">Sunday</div>
-  </div>
-  <div class="forecast-content">
-    <div class="forecast-icon">
-      <img src="images/icons/icon-14.svg" alt="" width=48>
-    </div>
-    <div class="degree">23<sup>o</sup>C</div>
-    <small>18<sup>o</sup></small>
-  </div>*/
